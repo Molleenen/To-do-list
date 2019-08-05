91bef4a13a75ec8511cf0e9dda39304f71c1ae74
Sebastian Wojciechowski <sebastian.wojciechowski.1998@gmail.com>
2019-08-05 19:16:51 +0200

    Add navigation bar to TaskListRootView.

--- To-do list/TaskList/View/TaskListRootView.swift
+++ To-do list/TaskList/View/TaskListRootView.swift
@@ -7,6 +7,19 @@ import UIKit
 
 class TaskListRootView: UIView {
     
+    private let navigationItem: UINavigationItem = {
+        let navigationItem = UINavigationItem(title: "To-do list")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        return navigationItem
+    }()
+    
+    private let navigationBar: UINavigationBar = {
+        let navigationBar = UINavigationBar()
+        navigationBar.barTintColor = .white
+        return navigationBar
+    }()
+    
     init() {
         super.init(frame: .zero)
     }
@@ -14,5 +27,32 @@ class TaskListRootView: UIView {
     @available(*, unavailable, message: "Use init() instead")
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func didMoveToWindow() {
+        backgroundColor = .white
+        setUpNavigatonBar()
+        constructHierarchy()
+        activateConstraints()
+    }
+    
+    private func setUpNavigatonBar() {
+        navigationBar.items = [navigationItem]
+    }
+    
+    private func constructHierarchy() {
+        addSubview(navigationBar)
+    }
+    
+    private func activateConstraints() {
+        activateConstraintsNavigationBar()
+    }
+    
+    private func activateConstraintsNavigationBar() {
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        let leading = navigationBar.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0)
+        let top = navigationBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0)
+        let trailing = navigationBar.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0)
+        NSLayoutConstraint.activate([top, leading, trailing])
     }
 }
