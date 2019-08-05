925ec72168262ace1815815f2badcd499bb4c8d8
Sebastian Wojciechowski <sebastian.wojciechowski.1998@gmail.com>
2019-08-05 19:11:23 +0200

    Use TaskListViewController as application RootViewController.

--- To-do list/AppDelegate.swift
+++ To-do list/AppDelegate.swift
@@ -10,7 +10,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let taskListView: TaskListRootView = TaskListRootView()
+        let taskListViewController: TaskListViewController = TaskListViewController(rootView: taskListView)
+        window?.rootViewController = taskListViewController
+        window?.makeKeyAndVisible()
         return true
     }
 }
