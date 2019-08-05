9edcdb63ad3e02e3a1a8e1f61fb917c7c98783ed
Sebastian Wojciechowski <sebastian.wojciechowski.1998@gmail.com>
2019-08-05 19:02:34 +0200

    Add initializers for view files.

--- To-do list/TaskList/View/TaskListRootView.swift
+++ To-do list/TaskList/View/TaskListRootView.swift
@@ -7,4 +7,12 @@ import UIKit
 
 class TaskListRootView: UIView {
     
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    @available(*, unavailable, message: "Use init() instead")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- To-do list/TaskList/View/TaskListViewController.swift
+++ To-do list/TaskList/View/TaskListViewController.swift
@@ -6,5 +6,19 @@
 import UIKit
 
 class TaskListViewController: UIViewController {
+    private let rootView: TaskListRootView
     
+    init(rootView: TaskListRootView) {
+        self.rootView = rootView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable, message: "Use init(rootView:) instead")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = rootView
+    }
 }
