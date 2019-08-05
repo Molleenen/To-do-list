a68903430fa70e5c098aab2cad3226cb280b8b6a
Sebastian Wojciechowski <sebastian.wojciechowski.1998@gmail.com>
2019-08-05 20:18:45 +0200

    Add CoreData data model file and code for persistent container and saving context.

--- To-do list/AppDelegate.swift
+++ To-do list/AppDelegate.swift
@@ -4,11 +4,22 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
 
     func application(
         _ application: UIApplication,
@@ -20,5 +31,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = taskListViewController
         window?.makeKeyAndVisible()
         return true
+    }
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
