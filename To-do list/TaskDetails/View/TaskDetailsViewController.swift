9822be3e82391d565b038e2baacb47d2889c0336
Sebastian Wojciechowski <sebastian.wojciechowski.1998@gmail.com>
2019-08-06 11:05:01 +0200

    Add initializers for TaskDetailsViewController.

--- To-do list/TaskDetails/View/TaskDetailsViewController.swift
+++ To-do list/TaskDetails/View/TaskDetailsViewController.swift
@@ -7,4 +7,19 @@ import UIKit
 
 class TaskDetailsViewController: UIViewController {
     
+    private let rootView: TaskDetailsRootVIew
+    
+    init(rootView: TaskDetailsRootVIew) {
+        self.rootView = rootView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable, message: "Use init(rootView:) instead")
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = rootView
+    }
 }
